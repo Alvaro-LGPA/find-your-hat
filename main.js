c1a4a16a4d837829e8abeb72e33985cde096a5dc
Alvaro-LGPA <alvarolaura@gmail.com>
2022-11-25 23:49:37 +0100

    Finish refactoring

--- main.js
+++ main.js
@@ -11,89 +11,91 @@ const pointer = {
 class Field {
     constructor(field) {
         this._field = field;
+        this._endGame = false;
+    }
+    clearConsole() {
+        console.clear(); // clear the console for a clean start with no header
     }
 
-    print() {
+    printField() { // prints the field
+        for (let i = 0; i < this._field.length; i++) {
+            console.log(this._field[i].join(""))
+        };
+    }
 
-        console.clear(); // clear the console for a clean start with no header
-        let endGame = false; // Loop will run until game is win or loose
 
-        while (endGame === false) {
 
-            for (let i = 0; i < this._field.length; i++) {
-                console.log(this._field[i].join(""))
-            };
 
-            //A method to handle asking and accepting user input, and updating the current location.
-            let cta;
-            cta = prompt("Which way?(u,d,l,r)")
+    startGame() { //A method to handle asking and accepting user input, and updating the current location.
+        let cta;
+        cta = prompt("Which way?(u,d,l,r)")
 
-            switch (cta) {
-                case "u":
-                    pointer.posV--; // move up
+        switch (cta) {
+            case "u":
+                pointer.posV--; // move up
+                break;
+            case "d":
+                pointer.posV++; // move down
+                break;
+            case "l":
+                pointer.posH--; // move left
+                break;
+            case "r":
+                pointer.posH++; // move right
+                break;
+            default:
+                break;
+        }
+        
+
+    };
+
+    evaluate() { // Methods to test whether the current location results in win (user is on the hat) or a loss (user is on a hole or out-of-bounds).
+        try {// going out of the field on the vertical axis throws and error. This try...catch statement handles it.
+            let newPosition = this._field[pointer.posV][pointer.posH]; // store array coordinates in a variable
+            console.log(`newPosition: ${newPosition}`);
+            switch (newPosition) {
+                case hat:
+                    this._endGame = true;
+                    prompt("You found your hat!!");
                     break;
-                case "d":
-                    pointer.posV++; // move down
+                case hole:
+                    this._endGame = true;
+                    prompt("You fell through a hole!! GAME OVER");
                     break;
-                case "l":
-                    pointer.posH--; // move left
+                case fieldCharacter:
+                    this._field[pointer.posV][pointer.posH] = pathCharacter; //updates curson new possition with *
                     break;
-                case "r":
-                    pointer.posH++; // move right
+                case undefined:
+                    this._endGame = true;
+                    prompt("You fell out of the field!! GAME OVER");
                     break;
+                case pathCharacter:
+                    this._endGame = true;
+                    prompt("You got lost!! GAME OVER")
                 default:
                     break;
             }
-
-            try {// going out of the field on the vertical axis throws and error. This try...catch statement handles it.
-                let newPosition = this._field[pointer.posV][pointer.posH]; // store array coordinates in a variable
-                switch (newPosition) { // Methods to test whether the current location results in win (user is on the hat) or a loss (user is on a hole or out-of-bounds).
-                    case hat:
-                        endGame = true;
-                        prompt("You found your hat!!");
-                        break;
-                    case hole:
-                        endGame = true;
-                        prompt("You fell through a hole!! GAME OVER");
-                        break;
-                    case fieldCharacter:
-                        this._field[pointer.posV][pointer.posH] = pathCharacter; //updates curson new possition with *
-                        break;
-                    case undefined:
-                        endGame = true;
-                        prompt("You fell out of the field!! GAME OVER");
-                        break;
-                    case pathCharacter:
-                        endGame = true;
-                        prompt("You got lost!! GAME OVER")
-                    default:
-                        break;
-                }
-            } catch (e) {
-                endGame = true;
-                prompt("You fell out of the field!! GAME OVER");
-            }
-            console.clear(); // refresh the console
+        } catch (e) {
+           this._endGame = true;
+           prompt("You fell out of the field!! Error GAME OVER");
+            
         }
+        
+    };
 
-        //update position
-
-        //A method to run the main game loop until the game is won or lost.
-
-        //To facilitate this, we also stored a horizontal and vertical player location on the class instance and initialize them in the constructor.
+    runGame() {
+        // Loop will run until game is win or loose
+        while (this._endGame === false) {
+            this.clearConsole();
+            this.printField();
+            this.startGame();
+            this.evaluate();      
+        }
+        
     }
 
-
-
-
-
-
 }
-
-
-// const generateField = (v, h) => {
-
-// }
 
 
 const myField = new Field([
@@ -102,4 +104,4 @@ const myField = new Field([
     ['░', '^', '░']
 ]);
 
-myField.print();
+myField.runGame();
